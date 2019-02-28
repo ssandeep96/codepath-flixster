905e7993612aec8aa98bebf1d81c5e271a828d1b
Sandeep Sagoo <ssagoo@ucsd.edu>
2019-02-27 23:51:27 -0800

    part2

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Sandeep Sagoo on 2/27/19.
+//  Copyright © 2019 Sandeep S Sagoo. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+}
